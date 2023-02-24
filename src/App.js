5965ee7ca7287e952c6c864e0b744b8931c8c67c
stormytuna <stormytuna@outlook.com>
2023-02-24 02:57:49 +0000

    Reroute unrouted paths to about page and add some info to about

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { Minesweeper } from "./games/minesweeper/components/Minesweeper";
 import { About } from "./components/About"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import "./styles/App.css";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <Route exact path="*" element={<Navigate to="/about" />} />
         <Route path="minesweeper" element={<Minesweeper />} />
         <Route path="about" element={<About />} />
       </Routes>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,10 @@
 export function About() {
 	return (
 		<div className="About">
-			<h1>Grahhhh</h1>
+			<h1>Hello there!</h1>
+			<p className="text-start px-5">This is a website that lets you waste time (who could've guessed)</p>
+			<p className="text-start px-5">Simply enter how long you want to waste in the top right and use the dropdown in the top left to select a game, or just wait, up to you</p>
+			<p className="text-start px-5">Idk what else to write here :DDDDD</p>
 		</div>
 	);
 }
